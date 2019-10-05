38cc9d02811d198981cc25fca6490c068bb58dd2
shigz97 <shigz97@gmail.com>
2019-10-05 22:03:58 +0800

    10-3

---
+++ p_152.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func maxProduct(nums []int) int {
+	max := 0
+	imax, imin := 1, 1
+
+}
+
+func main() {
+	nums := []int{2, 3, -2, 4}
+	fmt.Println(maxProduct(nums))
+}
